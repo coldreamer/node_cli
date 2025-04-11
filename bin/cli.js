a07aae397909c97786efb6cc2f9ac3d7cebde73b
johan_wu <johanoceans@gmail.com>
2025-04-11 08:19:36 +0800

    doc: add description

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,14 @@
 const { program } = require("commander");
 const myHelp = require("../lib/core/help");
 const myCommander = require("../lib/core/mycommander");
-
+// program: 操作指令的总对象
 program.version("1.0.0").description("johan cli");
 
+// 添加帮助指令
 myHelp(program);
+
+// 添加创建项目指令
 myCommander(program);
 
+// 解析命令行参数
 program.parse(process.argv);
