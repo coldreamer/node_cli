3c0eb8b13b5ea172706c45c5fef94d7b27ca6b4a
johan_wu <johanoceans@gmail.com>
2025-04-14 10:02:06 +0800

    feat: del file

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   framework: ["express", "react", "vue"],
   repo: {
-    express: "https://github.com/coldreamer/express-template.git",
+    express: "coldreamer/express-template",
     react: "https://github.com/coldreamer/react-template.git",
     vue: "https://github.com/coldreamer/vue-template.git",
   },
   projectName: "johan-cli",
-  projectPath: "./test/",
+  projectPath: "./",
 };
