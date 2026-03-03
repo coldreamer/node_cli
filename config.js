0daeaaf9dc79e02a8bb8168517a3255e36949130
johan_wu <johanoceans@gmail.com>
2026-03-03 11:04:47 +0800

    feat: add init tempalte

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
   framework: ["express", "react", "vue"],
+  projectType: ["Landing-Page (SSG)", "SaaS-App (SSR)"],
   repo: {
     express: "coldreamer/express-template",
     react: "https://github.com/coldreamer/react-template.git",
     vue: "https://github.com/coldreamer/vue-template.git",
+    "Landing-Page (SSG)": "coldreamer/react-website-template",
+    "SaaS-App (SSR)": "coldreamer/react-website-template",
   },
   projectName: "johan-cli",
   projectPath: "./",
